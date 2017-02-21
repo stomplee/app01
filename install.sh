8999cd1f2a2e430e3720be5d3b81fed04167fe0d
Ubuntu <ubuntu@ubuntu-yakkety.localdomain>
2017-02-21 20:05:46 +0000

    added initial installation and configuration script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt-get update
+apt-get install apache2 php libapache2-mod-php php-mcrypt php-mysql wget tar -y 
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+apt-get install mysql-server -y 
+wget http://sourceforge.net/projects/vtigercrm/files/vtiger%20CRM%206.5.0/Core%20Product/vtigercrm6.5.0.tar.gz
+tar -xvzf vtigercrm6.5.0.tar.gz
+cp -r vtigercrm/* /var/www/html/
+rm -f vtigercrm6.5.0.tar.gz
+rm -rf vtigercrm
+a2enmod ssl
+mkdir /etc/apache2/ssl
+mkdir /git
+git clone https://github.com/stomplee/app01 /git/app01
+chmod +X /git/app01/createdb.sh
+/git/app01/createdb.sh test user password
+cp /git/app01/apache.key /etc/apache2/ssl/
+cp /git/app01/apache.crt /etc/apache2/ssl/
+cp /git/app01/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+cp /git/app01/dir.conf /etc/apache2/mods-enabled/dir.conf
+chown -R www-data:www-data /etc/apache2
+chown -R www-data:www-data /var/www/html
+a2ensite default-ssl.conf
+service apache2 restart
