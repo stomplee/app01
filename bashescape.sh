6b1ebdd4d726c7a16cd99682213c5efbdb7538e9
Ubuntu <ubuntu@ubuntu-yakkety.localdomain>
2017-02-22 18:19:18 +0000

    removed bash escape test script

--- bashescape.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "mysql-server mysql-server/root_password password password" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password password" | debconf-set-selections
